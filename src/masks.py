f53c535d90487e34f423c73bee1c68034c6b0529
Suvorov.Alexandr <sasha.suvorov03@gmail.com>
2025-12-30 23:08:56 +1200

    task 10.2 done

---
+++ src/masks.py
@@ -0,0 +1,37 @@
+def get_mask_card_number(card_num: [str,int]) -> str:
+    """Принимает на вход номер карты и возвращает ее маску"""
+
+    if card_num is None:
+        return 'Incorrect data entry'
+    else:
+        str_card_num = str(card_num)
+
+        if str_card_num.__len__() > 16:
+            return 'Incorrect data entry'
+
+        mask = str_card_num.replace(str_card_num[6:12], "******")
+
+        space_card_num = ""
+        for i in mask:
+            space_card_num += i
+            if len(space_card_num) == 4 or len(space_card_num) == 9 or len(space_card_num) == 14:
+                space_card_num += " "
+
+        return space_card_num
+
+
+def get_mask_account(total_num: [str, int]) -> str:
+    """Принимает на вход номер счета и возвращает его маску"""
+
+    if total_num is None:
+        return 'Incorrect data entry'
+    else:
+        str_total_num = str(total_num)
+
+        if str_total_num.__len__() > 20:
+            return 'Incorrect data entry'
+
+        mask = str_total_num.replace(str_total_num[:-4], "**")
+
+        return mask
+
---
+++ tests/test_masks.py
@@ -0,0 +1,25 @@
+import pytest
+
+from src.masks import get_mask_card_number, get_mask_account
+
+
+@pytest.mark.parametrize('card, result', [
+    ('7000792289606361', '7000 79** **** 6361'),
+    ('5000892289605566', '5000 89** **** 5566'),
+    (None, 'Incorrect data entry'),
+    (5000892289605566, '5000 89** **** 5566'),
+    ('500089228960556623465', 'Incorrect data entry')
+])
+def test_get_mask_card_number(card, result):
+    assert get_mask_card_number(card) == result
+
+
+@pytest.mark.parametrize('account_num, result', [
+    ('73654108430135874305', '**4305'),
+    ('23593520050864052040', '**2040'),
+    (None, 'Incorrect data entry'),
+    (23593520050864052040, '**2040'),
+    ('82357934359238957235732', 'Incorrect data entry')
+])
+def test_get_mask_account(account_num, result):
+    assert get_mask_account(account_num) == result
