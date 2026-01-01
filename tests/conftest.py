ff9b1b45f57c7344668809fd20c649504ee86f7d
Suvorov.Alexandr <sasha.suvorov03@gmail.com>
2026-01-01 17:46:19 +1200

    all mistakes correct

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+@pytest.fixture
+def cards():
+    return '7000792289606361'
+
+@pytest.fixture
+def account():
+    return '73654108430135874305'
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,10 +1,10 @@
 import pytest
 
 from src.masks import get_mask_card_number, get_mask_account
+from tests.conftest import cards, account
 
 
 @pytest.mark.parametrize('card, result', [
-    ('7000792289606361', '7000 79** **** 6361'),
     ('5000892289605566', '5000 89** **** 5566'),
     (None, 'Incorrect data entry'),
     (5000892289605566, '5000 89** **** 5566'),
@@ -13,9 +13,11 @@ from src.masks import get_mask_card_number, get_mask_account
 def test_get_mask_card_number(card, result):
     assert get_mask_card_number(card) == result
 
+def test_get_mask_card_number_1(cards):
+    assert get_mask_card_number(cards) == '7000 79** **** 6361'
+
 
 @pytest.mark.parametrize('account_num, result', [
-    ('73654108430135874305', '**4305'),
     ('23593520050864052040', '**2040'),
     (None, 'Incorrect data entry'),
     (23593520050864052040, '**2040'),
@@ -23,3 +25,6 @@ def test_get_mask_card_number(card, result):
 ])
 def test_get_mask_account(account_num, result):
     assert get_mask_account(account_num) == result
+
+def test_get_mask_account_1(account):
+    assert get_mask_account(account) == '**4305'
