6b0d38449f741d9a636aee307a06008f5ec1dfae
Suvorov.Alexandr <sasha.suvorov03@gmail.com>
2026-02-16 23:46:28 +1200

    mistake

--- src/generators.py
+++ src/generators.py
@@ -20,17 +20,17 @@ def transaction_descriptions(transactions: list[dict]) -> Generator[Any, None]:
 
 
 def card_number_generator(start: int, finish: int) -> Any:
-    """ Функция, которая выдает номера банковских карт в формате XXXX ХХХХ ХХХХХ,
+    """Функция, которая выдает номера банковских карт в формате XXXX ХХХХ ХХХХХ,
      где X — цифра номера карты. Функция должна принимать начальное
     и конечное значения для генерации диапазона номеров."""
 
     for num in range(start, finish + 1):
         count_symbol_num = len(str(num))
-        card_num = ('0' * (16 - count_symbol_num)) + str(num)
+        card_num = ("0" * (16 - count_symbol_num)) + str(num)
 
-        a = card_num[:4] + ' '
-        b = card_num[4:8] + ' '
-        c = card_num[8:12] + ' '
+        a = card_num[:4] + " "
+        b = card_num[4:8] + " "
+        c = card_num[8:12] + " "
         d = card_num[12:]
 
         card_by_format = a + b + c + d
